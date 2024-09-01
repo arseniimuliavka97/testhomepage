6b4f9faa86c43f6ca82e3395027352941aacd22f
arseniimuliavka97 <142666084+arseniimuliavka97@users.noreply.github.com>
2024-09-01 20:41:11 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      boxShadow: {
+        custom: '0px 2px 20px 0px rgba(0, 18, 46, 0.18)',
+      },
+    },
+  },
+  plugins: [],
+}
+
